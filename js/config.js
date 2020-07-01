f4f4d1f5059edcf2dc7161d7b74827f8663e23d3
Christopher Lin <powertothepenguins@gmail.com>
2020-06-30 18:22:57 -0700

    add config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+(function closure(exports) {
+  exports.BASE_URL = 'http://127.0.0.1:8080';
+
+  // OR, if you have not set up a web server that runs the learning-opentok-php code,
+  // set these values to OpenTok API key, a valid session ID, and a token for the session.
+  // For test purposes, you can obtain these from https://tokbox.com/account.
+
+  exports.API_KEY = '46819664';
+  exports.SESSION_ID = '1_MX40NjgxOTY2NH5-MTU5MzU1OTg5MDgxMn5uNHhlZFd3Smhad2t1ZWRQZHVHenRxTE5-fg';
+  exports.TOKEN = 'T1==cGFydG5lcl9pZD00NjgxOTY2NCZzaWc9MWM0ZDlhODVmMDBkNWZhNTFkNGM2MDQ1NTZhOTY4ZWJiMWZlYjg2YjpzZXNzaW9uX2lkPTFfTVg0ME5qZ3hPVFkyTkg1LU1UVTVNelUxT1RnNU1EZ3hNbjV1TkhobFpGZDNTbWhhZDJ0MVpXUlFaSFZIZW5SeFRFNS1mZyZjcmVhdGVfdGltZT0xNTkzNTU5OTE0Jm5vbmNlPTAuMTY2MDgxMzY0NDE3OTA4MjImcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU5MzU4MTUxNCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
+})(exports);
